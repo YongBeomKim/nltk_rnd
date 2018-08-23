238152e9e960d81c5b40004836922c89d42b9595
YongBeomKim <saltman21@naver.com>
2018-08-23 12:08:19 +0900

    2th edited

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nltk_rnd
-서울R&amp;D캠퍼스 자연어 수업내용
+서울R&amp;D캠퍼스 자연어 수업자료
+
+**nltk 설치** 및 **Rule Based 관련 내용**에 대해 더 알고싶으신 분들은 [**nltk tutorial**](https://github.com/YongBeomKim/nltk_tutorial) 레포지토리 내용중 **3장** 슬라이드와(slideshare) Jupyter Notebook 소스자료를 참고하시면 됩니다.
--- py/txtutil.py
+++ py/txtutil.py
@@ -1,9 +1,5 @@
-# https://gist.github.com/himzzz/4105717
-
-
 # 텍스트를 줄단위로 끊어서 불러온뒤
 # Token 단위로, 한글명사들을 추출한다
-
 def txtnoun(filename , skip=False):
 
     try:
@@ -57,93 +53,7 @@ def txtnoun(filename , skip=False):
 
 
 
-def pdf_folder_txt(folder):
-
-    # 파일 최종 수정일
-    import os, glob
-    files     = sorted(glob.glob(folder), key=os.path.getmtime)
-    print(files[:5])
-
-    # 텍스트 파일에서 String 불러오기
-    texts = ''
-    for file in files:
-        f = open(file, 'r', encoding='utf-8')
-        texts += f.read()
-        f.close()
-
-    texts = texts.replace('\x0c','')
-    texts = texts.replace('\n\n\n','\n')
-    texts = texts.replace('\n\n','\n')
-    # 영문만 골라서 가져오기
-    # from nltk.tokenize import RegexpTokenizer
-    # if lang == 'en':
-    #     tokenize = RegexpTokenizer(r'[A-z]\w+')
-    # else : tokenize = RegexpTokenizer(r'[가-힣]\w+')
-    # texts_org = tokenize.tokenize(texts_org)
-    # result = ''
-    # for txt in texts_org:
-    #     result += ' ' + txt
-
-    filename = folder.split('/')[-2].replace('.pdf', '.txt')
-    with open('./data/'+filename, 'w', encoding='utf-8') as file:
-        file.write(texts)
-    file.close()
-    return texts[:100]
-    print(filename , 'is Saved')
-
-
-
-
-def txtnn(texts):
-    import re
-    texts  = texts.replace('\n', ' ')
-    hangul = re.compile(r'[^ ㄱ-힣]+') # 한글과 띄어쓰기를 제외한 모든 글자
-    result = hangul.sub('', texts)    # 한글과 띄어쓰기를 제외한 모든 부분을 제거
-
-    tokens = result.split(' ')
-    return tokens
-    from konlpy.tag import Twitter
-    twitter, result = Twitter(), []
-
-    for token in tokens:
-        temp = twitter.nouns(token)
-        if len("".join(temp)) > 1:
-            result.append("".join(temp))
-    return " ".join(result)
-
-
-
-def txt_to_nouns(texts, skipwords=False):
-    import re
-    texts  = texts.replace('\n', ' ')
-    #hangul = re.compile(r'[^ ㄱ-힣]+') # 한글과 띄어쓰기를 제외한 모든 글자
-    #result = hangul.sub('', texts)     # 한글과 띄어쓰기를 제외한 모든 부분을 제거
-    #tokens = result.split(' ')
-
-    tokenizer = re.compile(r'[가-힣]\w+') # 한글과 띄어쓰기를 제외한 모든 글자
-    texts = tokenizer.findall(texts)
-
-    from konlpy.tag import Twitter
-    twitter, result = Twitter(), []
-
-    # for token in tokens:
-    for token in texts:
-
-        if skipwords == True:
-            if token in ['갤럭시', '가치창출']:
-                result.append(token)
-
-        else:
-            temp = twitter.nouns(token)
-            if len("".join(temp)) > 1:
-                result.append("".join(temp))
-
-    return " ".join(result)
-
-
-
-
-# 테이블의 빈도를 대상으로 숫자를 변환하기
+# tf-idf 데이터 값들을 Rank 값으로 변환
 def table_rank(series):
     rank, result  = {}, {}
     # 순번 데이터 추출하기
@@ -159,6 +69,8 @@ def table_rank(series):
     return result.sort_values(ascending=False)
 
 
+# https://gist.github.com/himzzz/4105717
+# tf-idf 사용자 함수
 import re
 import nltk
 from nltk.tokenize import RegexpTokenizer
@@ -186,8 +98,8 @@ def idf(word, list_of_docs):
     return math.log(len(list_of_docs) /
             float(num_docs_containing(word, list_of_docs)))
 
-# 가운데가 많아질수록 값이 커진다
-# 가운데 Text가 doc이고
-# List 데이터가 words per doc 이 된다
+# doc : 가운데가 많아질수록 tfidf값이 커진다
+# doc : 분석대상 Text
+# list_of_docs : 분석 기준이 되는 token들의 모음
 def tf_idf(word, doc, list_of_docs):
     return (tf(word, doc) * idf(word, list_of_docs))
